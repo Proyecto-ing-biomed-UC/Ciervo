64143285916644f805fc21082eaa1338a845c213
carlos <cgvalle@uc.cl>
2024-07-17 23:53:20 -0400

    pipy setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.13' 
+DESCRIPTION = 'Ciervo for prosthetic leg control'
+LONG_DESCRIPTION = 'First version'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="ciervo", 
+        version=VERSION,
+        author="Carlos Valle",
+        author_email="cgvallea@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        package_data={'ciervo': ['tests/data/marcha/*.csv']},
+        include_package_data=True,
+
+
+)
