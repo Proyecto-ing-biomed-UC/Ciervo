a0e9a12e23e13469a3926a023f4f0fe262ef2abd
carlos <cgvalle@uc.cl>
2024-07-18 21:14:31 -0400

    downsample data

--- ciervo/io/load.py
+++ ciervo/io/load.py
@@ -5,48 +5,25 @@ import importlib
 from scipy import signal
 
 # Low pass filter
-sos = signal.butter(2, 5, 'lowpass', fs=1000, output='sos')
+sos = signal.butter(2, 5, 'lowpass', fs=250, output='sos')
 
 
 
 def load_csv(file_path):
     # df : Elapsed Time,Isquio,Cuadriceps,GLMedio,AductorLargo,Angle
     dtypes = {
-        "Elapsed Time": "str",
+        "Elapsed Time": "float",
         "Isquio": float,
         "Cuadriceps": float,
         "GLMedio": float,
         "AductorLargo": float,
         "Angle": float,
     }
-
+    
     # Load csv
     df = pd.read_csv(file_path)
-
-    # Replace Nan.NaN with np.nan
-    df = df.replace("NaN.NaN", 0)
-    df = df.replace("NaN.0", 0)
-
     # Set dtypes
     df = df.astype(dtypes)
-
-    # Interpolate nan values
-    for muscle in df.columns[1:-1]:
-        df[muscle] = np.interp(np.arange(len(df[muscle])), np.arange(len(df[muscle]))[~np.isnan(df[muscle])], df[muscle][~np.isnan(df[muscle])])
-
-
-
-
-
-
-    # Set datetime
-    df["Elapsed Time"] = np.linspace(0, len(df) / 1000, len(df))
-
-    # Filter angle
-    angle = np.array(df['Angle'])
-    angle = np.interp(np.arange(len(angle)), np.arange(len(angle))[~np.isnan(angle)], angle[~np.isnan(angle)])
-    angle = signal.sosfilt(sos, angle)
-    df['Angle'] = angle
 
     return df
 
