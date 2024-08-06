b9738eae00f2dd60d7bd6c3f21d0ac97d364ffaa
carlos <cgvalle@uc.cl>
2024-08-06 12:13:45 -0400

    plot arreglado para example_marcha_larga

--- ciervo/plots/emg.py
+++ ciervo/plots/emg.py
@@ -2,15 +2,21 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 
-def emg_plot(df: pd.DataFrame) -> None:
+def emg_plot(df: pd.DataFrame, start: float = 0.0, duration: float = 10) -> None:
     # df es un dataframe con las columnas Elapsed Time,Isquio,Cuadriceps,GLMedio,AductorLargo,Angle
-    # Se grafican las columnas Isquio,Cuadriceps,GLMedio,AductorLargo,Angle
+    # Por default solo grafica una ventana de 10 segundos desde el inicio.
 
-    fig, axs = plt.subplots(5, 1, figsize=(15, 14), sharex=True)
+    df = df[(df["Elapsed Time"] >= start) & (df["Elapsed Time"] <= start + duration)]
 
-    for i, col in enumerate(
-        ["Isquio", "Cuadriceps", "GLMedio", "AductorLargo", "Angle"]
-    ):
+
+
+
+    columns = list(df.columns)
+    # Remove elapsed time
+    columns.remove("Elapsed Time")
+
+    fig, axs = plt.subplots(len(columns), 1, figsize=(15, 14), sharex=True)    
+    for i, col in enumerate(columns):
         axs[i].plot(df["Elapsed Time"], df[col])
         axs[i].set_title(col)
 
