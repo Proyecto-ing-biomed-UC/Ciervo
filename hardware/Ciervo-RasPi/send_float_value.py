78565a901a7737fb7fe0a412d757aa0c4a52b396
Eoweo <camoro2002@gmail.com>
2024-09-21 02:33:00 -0300

    'update'

--- hardware/Ciervo-RasPi/send_float_value.py
+++ hardware/Ciervo-RasPi/send_float_value.py
@@ -1,9 +1,10 @@
 import serial
 import struct
 import time
+import random
 
 # Configurar el puerto serial (ajusta el puerto y baudrate según sea necesario)
-ser = serial.Serial('COM13', 9600)  # Reemplaza 'COM3' por tu puerto serial
+ser = serial.Serial('COM13', 9600, timeout=1)  # Reemplaza 'COM3' por tu puerto serial
 time.sleep(2)  # Espera a que se inicialice la conexión serial
 
 # Definir bytes especiales para inicio y término del mensaje
@@ -19,20 +20,38 @@ def send_float_via_serial(value):
     
     # Enviar el mensaje a través del puerto serial
     ser.write(message)
-    print(f"Enviado: {value} como mensaje: {message}")
+    print(f"Enviado: {value}")# como mensaje: {message}")
+
+def receive_message_from_arduino():
+    # Leer datos del Arduino si hay disponibles
+    if ser.in_waiting > 0:
+        # Leer la respuesta completa hasta encontrar un salto de línea
+        response = ser.readline().decode('utf-8').strip()
+        print(f"Mensaje recibido: {response}")
 
 # Ejemplo de uso
 try:
+    number = 0.0
+    numbers = [0.0, 127.0, 255.0]
+    i = 0
     while True:
         # Pide un valor flotante
-        number = float(input("Ingresa un número float para enviar: "))
+        #number += 0.1#float(input("Ingresa un número float para enviar: "))
+        number = numbers[i]
         
         # Enviar el float con bytes de inicio y término
         send_float_via_serial(number)
         
+        # Leer y mostrar los mensajes recibidos del Arduino
+        #receive_message_from_arduino()
+        
         # Esperar antes de enviar otro dato
-        time.sleep(1)
+        time.sleep(5)
+        i += 1
+        if i == len(numbers):
+            i = 0
 
 except KeyboardInterrupt:
     print("Terminando...")
     ser.close()
+
