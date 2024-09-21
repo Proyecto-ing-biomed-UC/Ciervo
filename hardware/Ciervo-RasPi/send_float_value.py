357a4b67e5ebdce8927e2c2fd7780d0b2f201b79
Eoweo <camoro2002@uc.cl>
2024-09-21 20:53:45 +0100

    update

--- hardware/Ciervo-RasPi/send_float_value.py
+++ hardware/Ciervo-RasPi/send_float_value.py
@@ -2,9 +2,11 @@ import serial
 import struct
 import time
 import random
+from threading import Thread
+import numpy as np
 
 # Configurar el puerto serial (ajusta el puerto y baudrate según sea necesario)
-ser = serial.Serial('COM13', 9600, timeout=1)  # Reemplaza 'COM3' por tu puerto serial
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # Reemplaza 'COM3' por tu puerto serial
 time.sleep(2)  # Espera a que se inicialice la conexión serial
 
 # Definir bytes especiales para inicio y término del mensaje
@@ -23,30 +25,42 @@ def send_float_via_serial(value):
     print(f"Enviado: {value}")# como mensaje: {message}")
 
 def receive_message_from_arduino():
-    # Leer datos del Arduino si hay disponibles
+    # leer datos del Arduino si hay disponibles
     if ser.in_waiting > 0:
         # Leer la respuesta completa hasta encontrar un salto de línea
-        response = ser.readline().decode('utf-8').strip()
+        response = ser.read_until(expected='\n'.encode('UTF-8')).decode('utf-8').strip()
         print(f"Mensaje recibido: {response}")
 
+def receive_message_thread():
+    while True:
+        receive_message_from_arduino()
+
+    
 # Ejemplo de uso
 try:
+    thread = Thread(target = receive_message_thread)
+    thread.start()
     number = 0.0
-    numbers = [0.0, 64.0, 127.0, 127.0 + 64.0, 255.0]
+    numbers = [0.0, 30.0, 60.0, 90.0, 120.0]
     i = 0
-    while True:
+
+    angle_data = np.genfromtxt('/home/eoweo/Ciervo/hardware/Ciervo-RasPi/data/angle.csv', delimiter=',')
+
+    for angle in angle_data:
+        
+        
         # Pide un valor flotante
         #number += 0.1#float(input("Ingresa un número float para enviar: "))
         number = numbers[i]
-        
+
         # Enviar el float con bytes de inicio y término
-        send_float_via_serial(number)
+        send_float_via_serial(angle)
         
         # Leer y mostrar los mensajes recibidos del Arduino
         #receive_message_from_arduino()
         
         # Esperar antes de enviar otro dato
-        time.sleep(0.1)
+        time.sleep(2)
         i += 1
         if i == len(numbers):
             i = 0
